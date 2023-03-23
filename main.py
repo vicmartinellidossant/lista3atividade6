90fb38cc89f1de118d584dbd79a1e4cda5f933ed
Victor Martinelli dos Santos <victoraugustomartinellidossant@gmail.com>
2023-03-22 22:50:41 -0300

    aula 22/03

---
+++ main.py
@@ -0,0 +1,8 @@
+priceUni = float(input("Qual é o preço de cada unidade do produto? "))
+
+quantVendas = int(input("Qual é a quantidade vendida? "))
+
+percentDescont = float(input("Qual é o percentual de desconto para a venda? "))
+
+sale = percentDescont / 100
+
