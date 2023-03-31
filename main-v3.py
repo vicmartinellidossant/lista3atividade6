e6bab9271c5a8ecb7faa3806b4ca9ce750b60797
vicmartinellidossant <128654932+vicmartinellidossant@users.noreply.github.com>
2023-03-31 12:27:24 -0300

    testanto

--- main-v3.py
+++ main-v3.py
@@ -5,3 +5,4 @@ desconto = float(input("Qual é o percentual de desconto para a venda? "))
 valorComDesconto = (precoUnit * qtde) * (1 - desconto / 100)
 
 print(f"O valor com desconto será R$ {valorComDesconto:.2f}")
+print("Deu certo: ")
